ff649d14e9f424910f66b774726827e767635766
Baptiste <baptiste.boiteux@cpe.fr>
2020-11-27 09:27:54 +0100

    fonction choix_mot

---
+++ P_main.py
@@ -0,0 +1,12 @@
+#Header 
+"""
+quoi :Programme principal projet pendu
+qui : Baptiste Boiteux
+quand : 27-11-2020
+"""
+
+#Importation des bibilothèques
+import p_fonctions as p
+
+mot = p.choix_mot()
+print(mot)
--- p_fonctions.py
+++ p_fonctions.py
@@ -1,8 +1,21 @@
-def decoupage_mots :
-    fichier = open("mots.txt")
-    liste_mots = []
-    for ligne in fichier :
-        mot =ligne.split(',')
-        liste_mots.append(mot)
-    return liste_mots
-    
+#Header 
+"""
+quoi : différentes fonctions du projet pendu
+qui : Baptiste Boiteux
+quand : 27-11-2020
+"""
+#Importation des bibilothèques
+
+import random as r
+
+def choix_mot() :
+    """
+    La fonction découpe le fichier mots.txt et choisi un mot au hasard
+    sortie : mot(str)
+    """
+    fichier = open("mots.txt",'r')
+    for ligne in fichier : 
+        liste_mots = ligne.split(',') #Séprartion des mots par par les virgule et ajout dnas la liste liste_mot
+    fichier.close()
+    mot = r.choice(liste_mots) #choix d'un mot au hasard dans la liste
+    return mot
