cab7b7f35371d1475728392d74a0fae092564eda
Baptiste <baptiste.boiteux@cpe.fr>
2020-12-11 12:22:52 +0100

    commit de fin

--- graphique.py
+++ graphique.py
@@ -3,19 +3,22 @@
 quoi :Programme du projet pendu graphique
 qui : Baptiste Boiteux
 quand : 11-12-2020
+TODO : faire la partie d'affichage des images
 """
 
 #Importation des bibilothèques
 import p_fonctions as p
-from tkinter import Tk, Label, Button, Canvas, Entry, StringVar,messagebox
+from tkinter import Tk, Label, Button, Canvas, Entry, StringVar,messagebox, PhotoImage,filedialog
 
 mot = p.choix_mot()
-
-
 l_entree = [mot[0]]
 """ ici je met la fonction verfication dans le programme directement car elle gère les elements graphique
 vu avec un professeur"""
+
 def Verification():
+    """
+    Met à jour les varibales d'affichage en détectant si la lettre est dans le mot
+    """
     global chance
     global entree
     global prop
@@ -33,6 +36,14 @@ def Verification():
         else :
             messagebox.showinfo("gagné","la lettre est dans le mot")
     prop.set(p.detection_lettre(mot,l_entree))
+    """if vie == 8 :
+        Canevas.delete('all') # on efface la zone graphique
+        photo = PhotoImage(file = bonhomme8.gif)
+        gifdict[bonhomme8.gif] = photo  # référence
+        print (gifdict)
+        Canevas.create_image(0,0,anchor = 'nw',image = photo)
+        Canevas.config(height = photo.height(),width = photo.width())
+        mw.title("Image " + str(photo.width()) + " x " + str(photo.height()))"""
 
 # création de la fenêtre graphique
 mw = Tk()
@@ -58,6 +69,8 @@ Label1.pack(side = 'bottom', padx = 5, pady = 5)
 # Création d'un widget Label (mot)
 Label1 = Label(mw, textvariable = prop)
 Label1.pack(side = 'right', padx = 5, pady = 5)
+# Utilisation d'un dictionnaire pour conserver une référence
+gifdict = {}
 #lancement du gestionnaire d'événements
 mw.mainloop()
 
