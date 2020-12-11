4390fe46de1ec5fc5fa89b1d40469c885154dd85
Baptiste <baptiste.boiteux@cpe.fr>
2020-12-11 08:24:27 +0100

    premier commit graphique

---
+++ graphique.py
@@ -0,0 +1,12 @@
+#Header 
+"""
+quoi :Programme du projet pendu graphique
+qui : Baptiste Boiteux
+quand : 11-12-2020
+"""
+
+#Importation des bibilothèques
+import p_fonctions as p
+from tkinter import Tk, Label, Button
+# création de la fenêtre graphique
+mw = Tk()
